125c5ada6e49068fceb7cf45bbeffd0b53bba013
sergii <koshevoys@gmail.com>
2022-12-10 17:59:02 +0200

    Revert "add hw lessons 25"
    This reverts commit 6ff64bab5f46088073997d4ee928dd96930c5c97.

--- Lesson_25/app/weather/models.py
+++
@@ -1,8 +0,0 @@
-from peewee import *
-
-from app.main.models import BaseModel
-
-
-class City(BaseModel):
-    country = CharField(max_length=100)
-    name = CharField(max_length=150, unique=True, index=True)
